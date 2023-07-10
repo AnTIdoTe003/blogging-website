40742d9e17c4af48e37d71ed271296975b0e6407
Debmalya Biswas <viperbale.db@gmail.com>
2023-07-10 11:51:52 +0530

    bug fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import Modal from "react-modal";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 type Props = {};
 
 interface Post {
@@ -19,7 +19,7 @@ interface Post {
 }
 
 const Dashboard = () => {
-  const router = useRouter()
+  // const router = useRouter();
   const session = useSession();
   const userName = session.data?.user?.name;
   const [formData, setFormData] = useState({
@@ -48,14 +48,17 @@ const Dashboard = () => {
     }
   };
 
-  const updatePost = async(id:any)=>{
-    const res = await axios.put(`/api/userPosts/${id}`, JSON.stringify(formData))
+  const updatePost = async (id: any) => {
+    const res = await axios.put(
+      `/api/userPosts/${id}`,
+      JSON.stringify(formData)
+    );
     if (res.status === 200) {
       toast.success("Updated Post Successfully");
     } else {
       toast.error("Something Went Wrong");
     }
-    }
+  };
 
   const createPost = async () => {
     const res = await axios.post("/api/userPosts", JSON.stringify(formData));
@@ -76,163 +79,171 @@ const Dashboard = () => {
       transform: "translate(-50%, -50%)",
       width: "500px",
       height: "500px",
-      background:"black",
-      borderRadius:"10px"
+      background: "black",
+      borderRadius: "10px",
     },
   };
   return (
     <>
-    {
-      (session.status === "authenticated")?  <div className="dashboard-wrapper">
-      <div className="dashboard-container">
-        <div className="dashboard-content">
-          <div className="dashboard-left">
-            {response.length !== 0 ? (
-              <>
-                <div>
-                  {response.map((ele: any) => {
-                    return (
-                      <div key={ele._id}>
-                        <p>{ele.title}</p>
-                        <Image
-                          src={ele.image}
-                          alt=""
-                          width={250}
-                          height={200}
-                        />
-                        <div className="dashboard-button-rack">
-                        <button onClick={openModal}>Edit</button>
-                        <button onClick={() => deletePost(ele._id)}>
-                          Delete
-                        </button>
-                        </div>
-                        <Modal
-                          style={customStyles}
-                          isOpen={isOpen}
-                          onRequestClose={closeModal}
-                        >
-                          <div className="dashboard-right">
-                            <form action="">
-                              <input
-                                onChange={(e) =>
-                                  setFormData({
-                                    ...formData,
-                                    title: e.target.value,
-                                  })
-                                }
-                                type="text"
-                                name=""
-                                id=""
-                                placeholder="Title"
-                              />
-                              <input
-                                onChange={(e) =>
-                                  setFormData({
-                                    ...formData,
-                                    content: e.target.value,
-                                  })
-                                }
-                                type="text"
-                                placeholder="Short Content"
-                              />
-                              <input
-                                onChange={(e) =>
-                                  setFormData({
-                                    ...formData,
-                                    description: e.target.value,
-                                  })
-                                }
-                                type="text"
-                                placeholder="The Blog"
-                              />
-                              <input
-                                onChange={(e) =>
-                                  setFormData({
-                                    ...formData,
-                                    image: e.target.value,
-                                  })
-                                }
-                                type="text"
-                                placeholder="Image Url"
-                              />
-                              <input
-                                onChange={(e) =>
-                                  setFormData({
-                                    ...formData,
-                                    name: e.target.value,
-                                  })
-                                }
-                                type="text"
-                                placeholder="Username"
-                              />
-                              <button onClick={()=>updatePost(ele._id)}>Update</button>
-                            </form>
+      {session.status === "authenticated" ? (
+        <div className="dashboard-wrapper">
+          <div className="dashboard-container">
+            <div className="dashboard-content">
+              <div className="dashboard-left">
+                {response.length !== 0 ? (
+                  <>
+                    <div>
+                      {response.map((ele: any) => {
+                        return (
+                          <div key={ele._id}>
+                            <p>{ele.title}</p>
+                            <Image
+                              src={ele.image}
+                              alt=""
+                              width={250}
+                              height={200}
+                            />
+                            <div className="dashboard-button-rack">
+                              <button onClick={openModal}>Edit</button>
+                              <button onClick={() => deletePost(ele._id)}>
+                                Delete
+                              </button>
+                            </div>
+                            <Modal
+                              style={customStyles}
+                              isOpen={isOpen}
+                              onRequestClose={closeModal}
+                            >
+                              <div className="dashboard-right">
+                                <form action="">
+                                  <input
+                                    onChange={(e) =>
+                                      setFormData({
+                                        ...formData,
+                                        title: e.target.value,
+                                      })
+                                    }
+                                    type="text"
+                                    name=""
+                                    id=""
+                                    placeholder="Title"
+                                  />
+                                  <input
+                                    onChange={(e) =>
+                                      setFormData({
+                                        ...formData,
+                                        content: e.target.value,
+                                      })
+                                    }
+                                    type="text"
+                                    placeholder="Short Content"
+                                  />
+                                  <input
+                                    onChange={(e) =>
+                                      setFormData({
+                                        ...formData,
+                                        description: e.target.value,
+                                      })
+                                    }
+                                    type="text"
+                                    placeholder="The Blog"
+                                  />
+                                  <input
+                                    onChange={(e) =>
+                                      setFormData({
+                                        ...formData,
+                                        image: e.target.value,
+                                      })
+                                    }
+                                    type="text"
+                                    placeholder="Image Url"
+                                  />
+                                  <input
+                                    onChange={(e) =>
+                                      setFormData({
+                                        ...formData,
+                                        name: e.target.value,
+                                      })
+                                    }
+                                    type="text"
+                                    placeholder="Username"
+                                  />
+                                  <button onClick={() => updatePost(ele._id)}>
+                                    Update
+                                  </button>
+                                </form>
+                              </div>
+                            </Modal>
                           </div>
-                        </Modal>
-                      </div>
-                    );
-                  })}
-                </div>
-              </>
-            ) : (
-              <>
-                <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"450px"}}>
-                  <p>You dont have any posts yet</p>
-                </div>
-              </>
-            )}
+                        );
+                      })}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "450px",
+                      }}
+                    >
+                      <p>You dont have any posts yet</p>
+                    </div>
+                  </>
+                )}
+              </div>
+              <div className="dashboard-right">
+                <form action="">
+                  <input
+                    onChange={(e) =>
+                      setFormData({ ...formData, title: e.target.value })
+                    }
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Title"
+                  />
+                  <input
+                    onChange={(e) =>
+                      setFormData({ ...formData, content: e.target.value })
+                    }
+                    type="text"
+                    placeholder="Short Content"
+                  />
+                  <input
+                    onChange={(e) =>
+                      setFormData({ ...formData, description: e.target.value })
+                    }
+                    type="text"
+                    placeholder="The Blog"
+                  />
+                  <input
+                    onChange={(e) =>
+                      setFormData({ ...formData, image: e.target.value })
+                    }
+                    type="text"
+                    placeholder="Image Url"
+                  />
+                  <input
+                    onChange={(e) =>
+                      setFormData({ ...formData, name: e.target.value })
+                    }
+                    type="text"
+                    placeholder="Username"
+                  />
+                  <button onClick={createPost}>Create</button>
+                </form>
+              </div>
+            </div>
           </div>
-          <div className="dashboard-right">
-            <form action="">
-              <input
-                onChange={(e) =>
-                  setFormData({ ...formData, title: e.target.value })
-                }
-                type="text"
-                name=""
-                id=""
-                placeholder="Title"
-              />
-              <input
-                onChange={(e) =>
-                  setFormData({ ...formData, content: e.target.value })
-                }
-                type="text"
-                placeholder="Short Content"
-              />
-              <input
-                onChange={(e) =>
-                  setFormData({ ...formData, description: e.target.value })
-                }
-                type="text"
-                placeholder="The Blog"
-              />
-              <input
-                onChange={(e) =>
-                  setFormData({ ...formData, image: e.target.value })
-                }
-                type="text"
-                placeholder="Image Url"
-              />
-              <input
-                onChange={(e) =>
-                  setFormData({ ...formData, name: e.target.value })
-                }
-                type="text"
-                placeholder="Username"
-              />
-              <button onClick={createPost}>Create</button>
-            </form>
-          </div>
+          <ToastContainer />
         </div>
-      </div>
-      <ToastContainer />
-    </div>
-    : router?.push('/dashboard/login')
-    }
-   
+      ) : (
+        window.location.replace('/dashboard/login')
+      )}
     </>
-    
   );
 };
 
